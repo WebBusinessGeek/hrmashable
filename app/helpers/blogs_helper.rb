580e4d747e21272796a2dee4f92c7a168c0bcd5a
Jony Malhotra <jony.malhotra82@gmail.com>
2013-08-07 16:55:49 +0530

    changed the way post time is displayed

--- app/helpers/blogs_helper.rb
+++ app/helpers/blogs_helper.rb
@@ -12,7 +12,7 @@ module BlogsHelper
                 reply.user.full_name if reply.user.present?
               end)
               concat(content_tag(:div, class: 'pull-right') do
-                "#{ Time.diff(Time.now, comment.updated_at, '%y, %M, %d, %H, %N, %S')[:diff] } ago"
+                last_updated_at(comment.updated_at)
               end)
             end)
             concat(content_tag(:div, class: 'row-fluid') do
@@ -23,20 +23,34 @@ module BlogsHelper
             concat(content_tag(:div, class: 'row-fluid') do
               concat(content_tag(:div, class: 'span6') do
                 concat(link_to 'Share', 'javascript:void(0);', :'data-toggle' => 'modal', :'data-target' => '#loginModal')
-                # concat(link_to_function 'Delete', "deleteComment(#{reply.id})", style: 'margin-left: 0.4em;')
-                concat(link_to 'Delete', delete_comment_blog_path(@blog), style: 'margin-left: 0.4em;', :'data-toggle' => 'modal', :'data-target' => '#loginModal')
+                concat(link_to_function 'Delete', "deleteComment(#{reply.id})", style: 'margin-left: 0.4em;')
               end)
               concat(content_tag(:div, class: 'pull-right') do
                 concat(link_to 'Like', 'javascript:void(0);', :'data-toggle' => 'modal', :'data-target' => '#loginModal')
                 concat(' / ')
-                # concat(link_to_function 'Reply', "replyComment(#{reply.id})")
-                concat(link_to 'Reply', post_comment_blog_path(@blog), :'data-toggle' => 'modal', :'data-target' => '#loginModal')
+                concat(link_to_function 'Reply', "replyComment(#{reply.id})")
               end)
             end)
             concat(get_all_children(reply))
           end)
         end)
       end.join)
+    end
+  end
+
+  def last_updated_at(time)
+    if Time.diff(Time.now, time)[:year] > 0
+      "#{Time.diff(Time.now, time, '%y')[:diff]} ago"
+    elsif Time.diff(Time.now, time)[:month] > 0
+      "#{Time.diff(Time.now, time, '%M')[:diff]} ago"
+    elsif Time.diff(Time.now, time)[:day] > 0
+      "#{Time.diff(Time.now, time, '%d')[:diff]} ago"
+    elsif Time.diff(Time.now, time)[:hour] > 0
+      "#{Time.diff(Time.now, time, '%H')[:diff]} ago"
+    elsif Time.diff(Time.now, time)[:minute] > 0
+      "#{Time.diff(Time.now, time, '%N')[:diff]} ago"
+    elsif Time.diff(Time.now, time)[:second] > 0
+      "#{Time.diff(Time.now, time, '%S')[:diff]} ago"
     end
   end
 end
