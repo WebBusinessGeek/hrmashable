d213372630d2b434eaca46208b5e9bc0a1a00110
Jony Malhotra <jony.malhotra82@gmail.com>
2013-08-22 19:12:47 +0530

    home page css changes

--- config/environment.rb
+++ config/environment.rb
@@ -4,8 +4,8 @@ require File.expand_path('../application', __FILE__)
 # Initialize the rails application
 HRResourceToday::Application.initialize!
 
-HRResourceToday::Application.config.middleware.use ExceptionNotification::Rack, :email => {
-  :email_prefix => "Exception Details: ",
-  :sender_address => %{"Exception Notifier" <notifier@grabsell.com>},
-  :exception_recipients => %w{kevw12188@gmail.com jony@idifysolutions.com}
-}
+# HRResourceToday::Application.config.middleware.use ExceptionNotification::Rack, :email => {
+#   :email_prefix => "Exception Details: ",
+#   :sender_address => %{"Exception Notifier" <notifier@grabsell.com>},
+#   :exception_recipients => %w{kevw12188@gmail.com jony@idifysolutions.com}
+# }
