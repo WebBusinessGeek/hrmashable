d59ba288a1d86bf25cb1736dc2e5a39ada1b518e
Jony Malhotra <jony.malhotra82@gmail.com>
2013-08-29 13:34:54 +0530

    fb omniauth key updated

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   # provider :facebook, '418496634933769', '74a62ca32fcd3dfa928dfff6912ba4ae'
   provider :google_oauth2, '395375844512.apps.googleusercontent.com', 's4Ns032tOSl8TRjir9GqiQ_L'
-  provider :facebook, '201431176692413', '6ab5d86aa6de69e6ab69c324fc6212dd'
+  provider :facebook, '194593790719157', 'aa02fe91ebf7be1ce6416e0cab5bf70a'
 end
