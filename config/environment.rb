0ed38d5b0e2076176f455fa7d49a03fb4014e6c2
Jony Malhotra <jony.malhotra82@gmail.com>
2013-09-03 11:52:23 +0530

    Exception Notifier re-configured, minor css update

--- config/environment.rb
+++ config/environment.rb
@@ -3,9 +3,3 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 HRResourceToday::Application.initialize!
-
-# HRResourceToday::Application.config.middleware.use ExceptionNotification::Rack, :email => {
-#   :email_prefix => "Exception Details: ",
-#   :sender_address => %{"Exception Notifier" <notifier@grabsell.com>},
-#   :exception_recipients => %w{kevw12188@gmail.com jony@idifysolutions.com}
-# }
---
+++ config/initializers/exception_notification.rb
@@ -0,0 +1,18 @@
+require 'exception_notification/rails'
+
+ExceptionNotification.configure do |config|
+  config.add_notifier :email, {
+    :email_prefix => "Exception Details: ",
+    :sender_address => %{"Exception Notifier" <notifier@hrmashable.com>},
+    :exception_recipients => %w{kevw12188@gmail.com jony@idifysolutions.com sahil@idifysolutions.com},
+    :delivery_method => :smtp,
+    :smtp_settings => {
+      :user_name => "myww",
+      :password => "myww1234",
+      :address => "smtp.sendgrid.net",
+      :port => 587,
+      :authentication => :plain,
+      :enable_starttls_auto => true
+    }
+  }
+end
