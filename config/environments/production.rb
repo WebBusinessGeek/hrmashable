ae0caba378c107171b202ef590197000995f69d3
Jony Malhotra <jony.malhotra82@gmail.com>
2013-09-03 11:46:22 +0530

    Exception Notifier re-configured, minor css update

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -50,9 +50,11 @@ HRResourceToday::Application.configure do
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
 
   # Enable threaded mode
-  # config.threadsafe!
+  config.threadsafe!
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation can not be found)
